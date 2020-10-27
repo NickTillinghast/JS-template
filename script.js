dff667e2861d81e2294de2f74d41f86b5311c3cb
Nick Tillinghast <nickt2@cox.net>
2020-10-27 08:42:50 -0600

    need new proxy

--- script.js
+++ script.js
@@ -3,19 +3,29 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
 
-
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 // Get Quote from API
 
 async function getQuote() {
+    showLoadingSpinner();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
         // IF author is blank, add "unkown"
-
         if (data.quoteAuthor === '') {
             authorText.innerText = 'Unknown';
 
@@ -30,7 +40,11 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
+        // stop loader, show quote
+        removeLoadingSpinner();
+        throw new Error('oops')
     } catch (error) {
+        console.log(error)
         getQuote();
 
     }
@@ -42,11 +56,8 @@ function tweetQuote() {
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
     window.open(twitterUrl, 'blank');
 }
-
 // event listeners
 newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote);
-
-
 // On load
 getQuote()
