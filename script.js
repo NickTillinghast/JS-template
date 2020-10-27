86e87bb60a3f63356144372cede66668b5e4c306
Nick Tillinghast <nickt2@cox.net>
2020-10-26 23:46:45 -0600

    error 429

---
+++ script.js
@@ -0,0 +1,52 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
+// Get Quote from API
+
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // IF author is blank, add "unkown"
+
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+    } catch (error) {
+        getQuote();
+
+    }
+}
+// tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, 'blank');
+}
+
+// event listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+// On load
+getQuote()
